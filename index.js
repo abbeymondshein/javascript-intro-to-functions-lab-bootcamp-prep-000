c7f23792f6beab155b036fdce2901d247859a57f
Abbey Mondshein <abbey.sync@gmail.com>
2018-08-12 13:23:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ mixedCase.toUpperCase() === mixedCase // false
 
 function sayHiToGrandma(string) {
  
-if (string === lowercase)
+if (string.toLowerCase() === lowercase)
 {return "I can't hear you!"}
 if (string === uppercase)
 {return "YES INDEED!"}
